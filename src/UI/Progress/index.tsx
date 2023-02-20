72a8c83d6861c7e158b6efbf03b304500508b38a
Razvan Tomegea <razvan.tomegea@gmail.com>
2023-02-20 13:05:11 +0200

    [+] Toast duration animation

--- src/UI/Progress/index.tsx
+++ src/UI/Progress/index.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useMemo, useRef, ReactNode } from 'react';
 import classNames from 'classnames';
-import { getUnixTimestampWithAddedSeconds } from 'utils/dateTime';
+import {
+  getUnixTimestamp,
+  getUnixTimestampWithAddedSeconds
+} from 'utils/dateTime';
 import { logarithmicRest } from 'utils/math';
 import { storage } from 'utils/storage';
 
@@ -32,7 +35,7 @@ export const Progress = ({
   const initialData = useMemo(() => {
     const totalSeconds = progress ? progress.endTime - progress.startTime : 0;
     const toastProgress = storage.session.getItem(TOAST_PROGRESS_KEY);
-    const unixNow = Math.floor(Date.now() / 1000);
+    const unixNow = getUnixTimestamp();
     const remaining = progress
       ? ((progress.endTime - unixNow) * 100) / totalSeconds
       : 0;
@@ -66,9 +69,8 @@ export const Progress = ({
   }, [progress, done]);
 
   function removeTxFromSession() {
-    const toastProgress: Record<number, number> = storage.session.getItem(
-      TOAST_PROGRESS_KEY
-    );
+    const toastProgress: Record<number, number> =
+      storage.session.getItem(TOAST_PROGRESS_KEY);
 
     const hasSessionStoredTx = Boolean(toastProgress?.[id]);
 
